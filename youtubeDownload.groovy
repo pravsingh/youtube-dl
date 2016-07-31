f0cfde4f3d30d1f9ddf4563eaa47d6707d5d563e
pravsingh <psinghiitr@gmail.com>
2016-07-31 00:21:49 -0700

    a

--- youtubeDownload.groovy
+++ youtubeDownload.groovy
@@ -30,23 +30,31 @@ jsonFile.eachLine { line ->
 
         vidTitleMap.put(videoJson.id, videoJson.title)
 
-	File audioFile = new File("temp_${videoJson.id}.mp4")
-	File videoFile = new File("temp_${videoJson.id}.m4a")
+        def audio = "temp_${videoJson.id}.audio"
+        def video = "temp_${videoJson.id}.video"
 
-	//download if not already downloaded	
-	if(!audioFile.exists() && !videoFile.exists()) {
-		downloadFile.append "\n\n\t    echo 'downloading[${count}] ${videoJson.id}' \n"
-		downloadFile.append "\t    rm -f *${videoJson.id}*\n"
-		downloadFile.append "\t    youtube-dl -f 140 ${videoJson.id} -o temp_${videoJson.id}.m4a &"  + "\n" //audio
-		downloadFile.append "\t    youtube-dl -f 134 ${videoJson.id} -o temp_${videoJson.id}.mp4 &"  + "\n" // video
+    	File audioFile = new File(audio)
+    	File videoFile = new File(video)
 
-		if(count % PARREL_DOWNLOADS == 0) {
-		   downloadFile.append "\n\n\t    wait \n"
-		}
+        
+    	//download if not already downloaded	
+    	if(!audioFile.exists()) {
+    		downloadFile.append "\t    rm -f ${audio}\n"
+            downloadFile.append "\t    youtube-dl -f 140 \'https://www.youtube.com/watch?v=${videoJson.id}\' -o ${audio} &"  + "\n" //audio
+            count++
+        }
 
-		count++
-	}
+        if( !videoFile.exists()) {
+            downloadFile.append "\t    rm -f ${video}\n"
+    		downloadFile.append "\t    youtube-dl -f 134 \'https://www.youtube.com/watch?v=${videoJson.id}\' -o ${video} &"  + "\n" // video
+            count++
+        }
+    	
+        if(count % PARREL_DOWNLOADS == 0) {
+    	   downloadFile.append "\n\n\t    wait \n"
+    	}
 
+    	
     }
 }
 
@@ -55,12 +63,19 @@ downloadFile.append "\n\n\t    wait \n"
 
 count = 0
 vidTitleMap.each { vid, title -> 
-    if(count % PARREL_CONVERSIONS == 0) {
-       downloadFile.append "\t    wait \n"
-    }
+
+    title = title.replaceAll("[^A-Za-z0-9 ]", "")
+
+    def fileName = "${title}.mp4"
+
+    downloadFile.append "\t    rm -f \'"+fileName+"\'\n"
+    
+     if(count % PARREL_CONVERSIONS == 0) {
+        downloadFile.append "\t    wait \n"
+     }
 
     //merge audio & video
-    downloadFile.append "\t    ffmpeg -i temp_${vid}.mp4 -i temp_${vid}.m4a -c:v copy -c:a aac -strict experimental \'${title}.mp4\' & \n"
+    downloadFile.append "\t    ffmpeg -i temp_${vid}.video -i temp_${vid}.audio -c:v copy -c:a aac -strict experimental \'${fileName}\'  \n"
 
     count++
 }
